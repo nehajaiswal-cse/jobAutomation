2b1d8ae92bba67be25d707928ea3153eeb680fd6
Neha Jaiswal <nehajaiawal840@gmail.com>
2026-05-13 17:19:08 +0530

    fixed broweser

--- backend/services/playwriteBot.js
+++ backend/services/playwriteBot.js
@@ -3,23 +3,28 @@ import chromium from "@sparticuz/chromium";
 
 export async function applyJobs(keyword,resumePath) {
 const browser = await puppeteer.launch({
-  args: chromium.args,
+   args: [
+      ...chromium.args,
+      "--no-sandbox",
+      "--disable-setuid-sandbox",
+    ],
+  defaultViewport: chromium.defaultViewport,  
   executablePath: await chromium.executablePath(),
-  headless: true,
+  headless: chromium.headless,
 });
   const page = await browser.newPage();
 
   // Example: Indeed (safe portal automation)
     await page.goto(
-    `https://in.indeed.com/jobs?q=${keyword}`
+    `https://in.indeed.com/jobs?q=${keyword}`,
+     {
+      waitUntil: "networkidle2",
+      timeout: 60000,
+    }
   );
-//  await page.setInputFiles(
-//   'input[type="file"]',
-//   resumePath
-// );
 
 
-  await page.waitForTimeout(Math.floor(Math.random() * 2000) + 1000);
+  await new Promise(resolve => setTimeout(resolve, Math.floor(Math.random() * 2000) + 1000));
   await page.waitForSelector(".jobTitle", { timeout: 60000 });
 
   const jobs = await page.$$eval(".jobTitle", nodes =>
