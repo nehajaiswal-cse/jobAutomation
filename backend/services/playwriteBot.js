3845543172a72d2c398359b1352d6854f11819ea
Neha Jaiswal <nehajaiawal840@gmail.com>
2026-05-13 19:19:06 +0530

    fixed issues

--- backend/services/playwriteBot.js
+++ backend/services/playwriteBot.js
@@ -1,42 +1,25 @@
-import puppeteer from "puppeteer-core";
-import chromium from "@sparticuz/chromium";
+import { chromium } from "playwright";
 
 export async function applyJobs(keyword) {
-
-  const browser = await puppeteer.launch({
-    args: [
-      ...chromium.args,
-      "--no-sandbox",
-      "--disable-setuid-sandbox",
-    ],
-    executablePath: await chromium.executablePath(),
-    headless: true,
+  const browser = await chromium.launch({
+    headless: false
   });
 
   const page = await browser.newPage();
 
-  await page.setUserAgent(
-    "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120 Safari/537.36"
+  await page.goto(`https://in.indeed.com ${keyword}`);
+
+  await page.fill("input[name='q']",);
+
+  await page.click("button[type='submit']");
+
+  await page.waitForTimeout(5000);
+
+  const jobs = await page.$$eval(".jobTitle", nodes =>
+    nodes.slice(0, 5).map(n => n.innerText)
   );
-
-  await page.goto(
-    `https://in.indeed.com/jobs?q=${encodeURIComponent(keyword)}`,
-    {
-      waitUntil: "networkidle2",
-      timeout: 60000,
-    }
-  );
-
-  // page title check
-  const title = await page.title();
-  console.log("PAGE TITLE:", title);
-
-  // full HTML check
-  const html = await page.content();
-
-  console.log(html.slice(0, 3000));
 
   await browser.close();
 
-  return [{ title }];
+  return jobs;
 }
