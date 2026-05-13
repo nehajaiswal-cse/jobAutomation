6399f7120794babefd3b5a8826e4c4ec1148a5bd
Neha Jaiswal <nehajaiawal840@gmail.com>
2026-05-13 16:31:58 +0530

    fixed render port

--- backend/app.js
+++ backend/app.js
@@ -17,6 +17,7 @@ mongoose.connect(process.env.MONGO_URL)
 
 app.use("/api/jobs", jobroutes);
 
-app.listen(5000, () => {
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
   console.log("Server running on port 5000");
 });
